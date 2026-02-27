d42048f17982252581e828a35965f3c6bb46ddcf
pip-lakshya <bhandarilakshya14@gmail.com>
2026-02-27 17:21:19 +0530

    changing-api

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -3,21 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDA-ZVbEo1PFYQ0ybj8QEcByKQWALoXC54",
-  authDomain: "helpline-manager.firebaseapp.com",
-  projectId: "helpline-manager",
-  storageBucket: "helpline-manager.firebasestorage.app",
-  messagingSenderId: "412306351933",
-  appId: "1:412306351933:web:9d83e7f0fd3dfdc2bec754",
-  measurementId: "G-JS7V2ZBZNF"
+  apiKey: "Your api key here",
+  authDomain: "Your auth domain here",
+  projectId: "Your project id here",
+  storageBucket: "Your storage bucket here",
+  messagingSenderId: "Your sender id here",
+  appId: "Your app id here",
+  measurementId: "Your measurement id here",
 };
 
-const app = getApps().length === 0
-  ? initializeApp(firebaseConfig)
-  : getApps()[0];
+const app =
+  getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-
-
